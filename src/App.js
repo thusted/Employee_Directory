4cbfc62d06c9f0e91ef08d3acf06ab2de555194d
Tiana Husted <tiana.husted@gmail.com>
2020-09-22 16:07:13 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Employee Directory
+      </header>
+    </div>
+  );
+}
+
+export default App;
