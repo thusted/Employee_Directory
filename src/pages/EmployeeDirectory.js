18c11152d7d105ed0efd586d7fb0aa265fb27b0b
Tiana Husted <tiana.husted@gmail.com>
2020-09-23 14:24:18 -0700

    Converting display to grid layout

--- src/pages/EmployeeDirectory.js
+++ src/pages/EmployeeDirectory.js
@@ -38,9 +38,7 @@ class EmployeeDirectory extends Component {
       <div>
         <Container style={{ minHeight: "80%" }}>
           <h1 className="text-center">Employee Directory</h1>
-          <p>
-            Your search is {this.state.search.toLowerCase()}
-          </p>
+
           <input
             value={this.state.search.toLowerCase()}
             name="search"
@@ -49,16 +47,28 @@ class EmployeeDirectory extends Component {
             placeholder="Type a name of an employee"
           />
         
-          {employees.filter(employee => employee.name.toLowerCase().includes(this.state.search)).map(filteredEmployee => (
-            <ul>
-              <img alt="employeeImg" src={filteredEmployee.image}/>
-              <li>Name: {filteredEmployee.name}</li>
-              <li>Country: {filteredEmployee.country}</li>
-              <li>Phone: {filteredEmployee.phone}</li>
-              <li>Email: {filteredEmployee.email}</li>
-            </ul>
-          ))}
-
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Image</th>
+                <th scope="col">Name</th>
+                <th scope="col">Country</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Email</th>
+              </tr>
+            </thead>
+            {employees.filter(employee => employee.name.toLowerCase().includes(this.state.search)).map(filteredEmployee => (
+              <tbody>
+                <tr>
+                  <th scope="row"><img alt="employeeImg" src={filteredEmployee.image}/></th>
+                  <td>{filteredEmployee.name}</td>
+                  <td>{filteredEmployee.country}</td>
+                  <td>{filteredEmployee.phone}</td>
+                  <td>{filteredEmployee.email}</td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
         </Container>
       </div>
     );
